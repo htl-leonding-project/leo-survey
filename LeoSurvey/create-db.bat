c34003fa72ca9a6597c00a2933fe8ce9b260a42b
FabianTischler <fabian.tischler.ft@gmail.com>
2021-01-12 14:19:32 +0100

    TF, 11.01.2021, changed Questionnaire for db, cleaned Project for db #4

---
+++ LeoSurvey/create-db.bat
@@ -0,0 +1,5 @@
+mkdir db
+cd db
+curl https://apache.mirror.digionline.de//db/derby/db-derby-10.15.2.0/db-derby-10.15.2.0-bin.zip -o derbydb.zip
+unzip derbydb.zip
+
--- LeoSurvey/src/main/java/at/htl/leosurvey/entities/Questionnaire.java
+++ LeoSurvey/src/main/java/at/htl/leosurvey/entities/Questionnaire.java
@@ -1,27 +1,28 @@
 package at.htl.leosurvey.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Questionnaire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int qn_id;
     private String qn_name;
     private String qn_description;
-    private Question[] qn_questions;
 
     public Questionnaire() {
     }
 
-    public Questionnaire(int qn_id, String qn_name, String qn_description, Question[] qn_questions) {
-        this.qn_id = qn_id;
+    public Questionnaire(String qn_name, String qn_description) {
         this.qn_name = qn_name;
         this.qn_description = qn_description;
-        this.qn_questions = qn_questions;
     }
 
     public int getQn_id() {
         return qn_id;
-    }
-
-    public void setQn_id(int qn_id) {
-        this.qn_id = qn_id;
     }
 
     public String getQn_name() {
@@ -40,11 +41,4 @@ public class Questionnaire {
         this.qn_description = qn_description;
     }
 
-    public Question[] getQn_questions() {
-        return qn_questions;
-    }
-
-    public void setQn_questions(Question[] qn_questions) {
-        this.qn_questions = qn_questions;
-    }
 }
