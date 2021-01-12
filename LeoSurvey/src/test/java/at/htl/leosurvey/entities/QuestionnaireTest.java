cf49525717b385a438fa3fb53f8a1946b0c411ed
FabianTischler <fabian.tischler.ft@gmail.com>
2021-01-12 14:20:47 +0100

    TF, 11.01.2021, added IntegrationTest Questionnaire for db #6

---
+++ LeoSurvey/src/test/java/at/htl/leosurvey/entities/QuestionnaireTest.java
@@ -0,0 +1,52 @@
+package at.htl.leosurvey.entities;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.assertj.db.type.Table;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import javax.transaction.*;
+
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import org.apache.derby.jdbc.*;
+
+@QuarkusTest
+class QuestionnaireTest {
+    @Inject
+    EntityManager em;
+    @Inject
+    UserTransaction tm;
+
+
+    static final String DATABASE = "db";
+    static final String USERNAME = "app";
+    static final String PASSWORD = "app";
+    public static final String URL = "jdbc:derby://localhost:1527/db";
+
+    public static DataSource getDataSource() {
+        ClientDataSource dataSource = new ClientDataSource();
+        dataSource.setDatabaseName(DATABASE);
+        dataSource.setUser(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        return dataSource;
+    }
+
+    @Test
+    void createAQuestionnaireTest() throws SystemException, NotSupportedException,
+            HeuristicRollbackException, HeuristicMixedException, RollbackException {
+        Questionnaire q = new Questionnaire("Test", "Test of the Questionnaire");
+        tm.begin();
+        em.persist(q);
+        tm.commit();
+        Table questionnaires = new Table(getDataSource(), "questionnaire");
+        assertThat(questionnaires).column("qn_id")
+                .value().isEqualTo(1)
+                .column("qn_name")
+                .value().isEqualTo("Test")
+                .column("qn_description")
+                .value().isEqualTo("Test of the Questionnaire");
+    }
+}
