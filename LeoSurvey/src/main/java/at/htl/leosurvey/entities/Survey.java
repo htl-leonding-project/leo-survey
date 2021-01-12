55038cce26f973418af3fb3b1a654d3600135739
FabianTischler <fabian.tischler.ft@gmail.com>
2021-01-12 15:01:44 +0100

    TF, 12.01.2021, changed Entity Survey for db #4

--- LeoSurvey/src/main/java/at/htl/leosurvey/entities/Survey.java
+++ LeoSurvey/src/main/java/at/htl/leosurvey/entities/Survey.java
@@ -1,47 +1,27 @@
 package at.htl.leosurvey.entities;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
 public class Survey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int s_id;
-    private S_Transaction s_transaction;
-    private Teacher s_teacher;
     private LocalDate s_date;
+    @ManyToOne(cascade = CascadeType.ALL)
     private Questionnaire s_questionnaire;
 
     public Survey() {
     }
 
-    public Survey(int s_id, S_Transaction s_transaction, Teacher s_teacher, LocalDate s_date, Questionnaire s_questionnaire) {
-        this.s_id = s_id;
-        this.s_transaction = s_transaction;
-        this.s_teacher = s_teacher;
+    public Survey(LocalDate s_date, Questionnaire s_questionnaire) {
         this.s_date = s_date;
         this.s_questionnaire = s_questionnaire;
     }
 
     public int getS_id() {
         return s_id;
-    }
-
-    public void setS_id(int s_id) {
-        this.s_id = s_id;
-    }
-
-    public S_Transaction getS_transaction() {
-        return s_transaction;
-    }
-
-    public void setS_transaction(S_Transaction s_transaction) {
-        this.s_transaction = s_transaction;
-    }
-
-    public Teacher getS_teacher() {
-        return s_teacher;
-    }
-
-    public void setS_teacher(Teacher s_teacher) {
-        this.s_teacher = s_teacher;
     }
 
     public LocalDate getS_date() {
