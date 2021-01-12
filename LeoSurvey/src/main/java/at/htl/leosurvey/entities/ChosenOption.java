4ad834751e57d60633a2daf2a7c598fd1c933811
FabianTischler <fabian.tischler.ft@gmail.com>
2021-01-12 14:56:01 +0100

    TF, 12.01.2021, added Entity ChosenOption #4

---
+++ LeoSurvey/src/main/java/at/htl/leosurvey/entities/ChosenOption.java
@@ -0,0 +1,53 @@
+package at.htl.leosurvey.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class ChosenOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long co_id;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private AnswerOption co_ao;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Answer co_a;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Question co_q;
+
+    public ChosenOption() {
+    }
+
+    public ChosenOption(AnswerOption co_ao, Answer co_a, Question co_q) {
+        this.co_ao = co_ao;
+        this.co_a = co_a;
+        this.co_q = co_q;
+    }
+
+    public Long getCo_id() {
+        return co_id;
+    }
+
+    public AnswerOption getCo_ao() {
+        return co_ao;
+    }
+
+    public void setCo_ao(AnswerOption co_ao) {
+        this.co_ao = co_ao;
+    }
+
+    public Answer getCo_a() {
+        return co_a;
+    }
+
+    public void setCo_a(Answer co_a) {
+        this.co_a = co_a;
+    }
+
+    public Question getCo_q() {
+        return co_q;
+    }
+
+    public void setCo_q(Question co_q) {
+        this.co_q = co_q;
+    }
+}
