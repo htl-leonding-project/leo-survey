3b3e68deefebad9f8d3e1d84b354ac37f8a81467
PhilipTrinkl <philip.trinkl@gmail.com>
2021-01-19 13:56:49 +0100

    TP, 19.01.2020, created QuestionnaireRepositoryTest with create Test method #5

--- LeoSurvey/src/main/java/at/htl/leosurvey/control/QuestionnaireRepository.java
+++ LeoSurvey/src/main/java/at/htl/leosurvey/control/QuestionnaireRepository.java
@@ -4,14 +4,17 @@ import at.htl.leosurvey.entities.Questionnaire;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class QuestionnaireRepository implements PanacheRepository<Questionnaire> {
 
+    @Transactional
     public Questionnaire save(Questionnaire questionnaire){
         return getEntityManager().merge(questionnaire);
     }
 
+    @Transactional
     public void delete(long id){
         delete(find("id",id).singleResult());
     }
---
+++ LeoSurvey/src/test/java/at/htl/leosurvey/control/QuestionnaireRepositoryTest.java
@@ -0,0 +1,44 @@
+package at.htl.leosurvey.control;
+
+import at.htl.leosurvey.entities.Questionnaire;
+import io.quarkus.test.junit.QuarkusTest;
+import org.assertj.db.type.Table;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import at.htl.leosurvey.misc.DataSource;
+
+import javax.inject.Inject;
+import javax.transaction.UserTransaction;
+
+import static org.assertj.db.api.Assertions.assertThat;
+
+@QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class QuestionnaireRepositoryTest {
+
+    @Inject
+    QuestionnaireRepository questionnaireRepository;
+
+    @Inject
+    UserTransaction userTransaction;
+
+    @Test
+    @Order(10)
+    void createQuestionnaireTest(){
+        Questionnaire q = new Questionnaire("Test", "Test of the Questionnaire");
+        questionnaireRepository.save(q);
+
+
+        Table t = new Table(DataSource.getDataSource(), "questionnaire");
+        assertThat(t).column("qn_id")
+                .value().isEqualTo(1)
+                .column("qn_name")
+                .value().isEqualTo("Test")
+                .column("qn_description")
+                .value().isEqualTo("Test of the Questionnaire");
+
+    }
+
+}
