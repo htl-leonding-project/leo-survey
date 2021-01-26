9556e0dbacbe68882b5004b7774915b66e2750c6
PhilipTrinkl <philip.trinkl@gmail.com>
2021-01-26 14:35:52 +0100

    TP, 26.01.2021, created TeacherRepositoryTest

---
+++ LeoSurvey/src/test/java/at/htl/leosurvey/control/TeacherRepositoryTest.java
@@ -0,0 +1,39 @@
+package at.htl.leosurvey.control;
+
+import at.htl.leosurvey.entities.Questionnaire;
+import at.htl.leosurvey.entities.Survey;
+import at.htl.leosurvey.entities.Teacher;
+import at.htl.leosurvey.misc.DataSource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.assertj.db.type.Table;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import javax.inject.Inject;
+import java.time.LocalDate;
+
+import static org.assertj.db.api.Assertions.assertThat;
+
+@QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TeacherRepositoryTest {
+
+    @Inject
+    TeacherRepository teacherRepository;
+
+    @Test
+    @Order(10)
+    void createTeacherTest(){
+        Questionnaire q = new Questionnaire("Test", "Test of the Questionnaire");
+        LocalDate dt = LocalDate.now();
+        Survey s = new Survey(dt, q);
+        teacherRepository.save(new Teacher("Teach", s));
+        Table teacher = new Table(DataSource.getDataSource(), "teacher");
+        assertThat(teacher).row(0)
+                .value().isEqualTo(1)
+                .value().isEqualTo("Teach")
+                .value().isEqualTo(1);
+    }
+}
