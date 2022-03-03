8ee327a219629c094008fead42c81bcb3e3e13da
= <fabian.tischler.ft@gmail.com>
2022-03-03 11:44:36 +0100

    TF, 03.03.2022, added methods for transaction geeration #LS-8

--- LeoSurvey/src/main/java/at/htl/leosurvey/boundary/SurveyEndpoint.java
+++ LeoSurvey/src/main/java/at/htl/leosurvey/boundary/SurveyEndpoint.java
@@ -1,6 +1,9 @@
 package at.htl.leosurvey.boundary;
 
+import at.htl.leosurvey.control.QuestionnaireRepository;
+import at.htl.leosurvey.control.S_TransactionRepository;
 import at.htl.leosurvey.control.SurveyRepository;
+import at.htl.leosurvey.entities.Questionnaire;
 import at.htl.leosurvey.entities.Survey;
 
 import javax.inject.Inject;
@@ -9,6 +12,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.time.LocalDate;
 import java.util.List;
 
 @Path("leosurvey")
@@ -16,12 +20,32 @@ public class SurveyEndpoint {
     @Inject
     SurveyRepository surveyRepository;
 
+    @Inject
+    S_TransactionRepository transactionRepository;
+
+    @Inject
+    QuestionnaireRepository questionnaireRepository;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/survey")
     public Response findAllSurveys(){
         final List<Survey> surveys = surveyRepository.findAllSurveys();
         return Response.ok(surveys).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/createSurvey")
+    public Response createTransactions(int amount){
+        Questionnaire questionnaire = questionnaireRepository.findById(1);
+        Survey survey1 = new Survey(LocalDate.now(),questionnaire);
+        final List<String> transactions = this.transactionRepository.generateTransactionCode(survey1,amount);
+        surveyRepository.save(survey1);
+        return Response
+                .ok()
+                .entity(transactions)
+                .build();
     }
 
 }
--- LeoSurvey/src/main/java/at/htl/leosurvey/control/S_TransactionRepository.java
+++ LeoSurvey/src/main/java/at/htl/leosurvey/control/S_TransactionRepository.java
@@ -1,10 +1,14 @@
 package at.htl.leosurvey.control;
 
 import at.htl.leosurvey.entities.S_Transaction;
+import at.htl.leosurvey.entities.Survey;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 @ApplicationScoped
 public class S_TransactionRepository implements PanacheRepository<S_Transaction> {
@@ -12,5 +16,28 @@ public class S_TransactionRepository implements PanacheRepository<S_Transaction>
      @Transactional
      public S_Transaction save(S_Transaction s_transaction){
           return getEntityManager().merge(s_transaction);
+     }
+
+     @Transactional
+     public List<String> generateTransactionCode(Survey survey, int amount){
+          final LinkedList<String> transactions = new LinkedList<>();
+
+
+          Random r = new Random();
+          String back = "";
+          for(int i = 0; i <= amount; i++){
+               for(int o = 0; o < 16; o++){
+                    char c = (char)(r.nextInt(26) + 'a');
+                    back += c;
+               }
+               S_Transaction transaction = new S_Transaction();
+               transaction.setT_transactioncode(back);
+               transaction.setT_is_used(false);
+               transaction.setT_survey(survey);
+               getEntityManager().persist(transaction);
+               transactions.add(transaction.getT_transactioncode());
+               back = "";
+          }
+          return transactions;
      }
 }
